5a93d97e8e5c49e59a6e5cf49f4bd8783daa3486
prystapchuk-dev <prystapchuk@gmail.com>
2022-03-31 15:49:09 +0300

    next commit

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -2,28 +2,49 @@ import {createRouter, createWebHistory} from "vue-router";
 import Dashboard from '../views/Dashboard.vue';
 import Login from '../views/Login.vue';
 import Register from '../views/Register.vue';
+import DefaultLayout from "../components/DefaultLayout.vue";
+import AuthLayout from "../components/AuthLayout.vue";
+import Surveys from "../views/Surveys.vue";
+import store from "../store";
 
 const routes = [
   {
     path: '/',
-    name: 'Dashboard',
-    component: Dashboard
+    redirect: '/dashboard',
+    component: DefaultLayout,
+    meta: {requiresAuth: true},
+    children: [
+      {path: '/dashboard', name: 'Dashboard', component: Dashboard},
+      {path: '/surveys', name: 'Surveys', component: Surveys},
+    ]
   },
   {
-    path: '/login',
-    name: 'Login',
-    component: Login
-  },
-  {
-    path: '/register',
-    name: 'Register',
-    component: Register
+    path: '/auth',
+    redirect: '/login',
+    name: 'Auth',
+    component: AuthLayout,
+    meta: {isGuest: true},
+    children: [
+      {path: '/login', name: 'Login', component: Login},
+      {path: '/register', name: 'Register', component: Register},
+    ]
   },
 ];
 
 const router = createRouter({
   history: createWebHistory(),
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.requiresAuth && !store.state.user.token) {
+    console.log(to.meta.requiresAuth);
+    next({name: 'Login'});
+  } else if (store.state.user.token && to.meta.isGuest) {
+    next({name: 'Dashboard'});
+  } else {
+    next();
+  }
 })
 
 
